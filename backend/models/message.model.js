b87e505e139539f30fd5c4371059a0ec4517cae0
SangameshwarGaikwad21 <sangamgaikwad214@gmail.com>
2026-07-23 10:28:09 +0530

    added the message model

---
+++ backend/models/message.model.js
@@ -0,0 +1,38 @@
+import mongoose,{Schema} from "mongoose"
+
+const messageSchema = new Schema(
+    {
+        conversation: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true,
+        },
+        sender:{
+            type:mongoose.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        receiver:{
+            type:mongoose.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        text:{
+            type: String,
+            trim: true,
+        },
+        image:{
+            type: String,
+            trim: true,
+        },
+        isSeen:{
+            type:Boolean,
+            defulat:false
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Message = mongoose.model("Message",messageSchema)
