c91b20b089a4716135d08089ef6664e98c427c97
SangameshwarGaikwad21 <sangamgaikwad214@gmail.com>
2026-07-28 16:39:41 +0530

    aded the react-redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Routes>
 
-    </div>
+      </Routes>
+    </>
   )
 }
 
